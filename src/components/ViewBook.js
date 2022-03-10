4128954b0804783eb069de123c7207d84e16376b
Ben Corbin <ben@corbin.digital>
2022-03-10 12:45:49 +0000

    Changed from using useLocation to useParams

--- src/components/ViewBook.js
+++ src/components/ViewBook.js
@@ -4,14 +4,23 @@ import { useParams, useLocation } from "react-router-dom"
 function ViewBook() {
   const [book, setBook] = useState(false)
 
-  const location = useLocation()
+  // const location = useLocation()
 
-  useEffect(() =>{
-    if(location.state) {
-      setBook(location.state.book)
-    }
-  }, [location])
+  // useEffect(() =>{
+  //   if(location.state) {
+  //     setBook(location.state.book)
+  //   }
+  // }, [location])
 
+  const params = useParams()
+  useEffect(function(){
+    fetch(`http://localhost:4000/books/${params.id}`)
+    .then(res => res.json())
+    .then(json => {
+      setBook(json)
+    })
+  },[params])
+  
   if (!book) {
     return <p>Loading</p>
   }
